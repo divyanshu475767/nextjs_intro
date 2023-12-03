791981dc7e8ec5010293e92b1cba90185aac7836
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-12-03 15:44:05 +0530

    Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
+
+import Link from "next/link"
+
+
+
 export default function News() {
     return (
       <>
       The news page
+      <ul>
+        <li>
+          <Link href='/news/Nextjs'>
+          NextJs is a Great Framework
+          </Link>
+          </li>
+        <li>
+          <Link href='/news/reactjs'>
+          Reacts is a Great Library
+          </Link>
+          </li>
+      </ul>
       </>
     )
   }
